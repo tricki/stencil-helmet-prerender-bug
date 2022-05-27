7dd762803790210906537f3632cff1a5196b2651
tricki <tricki@users.noreply.github.com>
2022-05-27 11:49:57 +0200

    fix: empty directory before build
    Fixes "file not found" errors.

--- stencil.config.ts
+++ stencil.config.ts
@@ -13,6 +13,7 @@ export const config: Config = {
       // comment the following line to disable service workers in production
       serviceWorker: null,
       baseUrl: 'https://myapp.local/',
+      empty: true,
     },
   ],
 };
